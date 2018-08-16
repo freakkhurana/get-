ee9a4f112e03f61b40bde2025e067f9fc3415a5c
freakkhurana <41017109+freakkhurana@users.noreply.github.com>
2018-08-16 22:58:48 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 var express = require('express');
-var todoController = require('./controllers/todoController');
+var path = require('path');
+var favicon = require('server-favicon');
+var logger = require('morgan');
+var body-parser = require('body-parser');
 
+var mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://test:testify123@ds113452.mlab.com:13452/todoapp', { useMongoClient: true, promiseLibrary: require('bluebird')});
+
+var book = require('./routes/book');
 var app = express();
 
 
-app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(body-parser.json());
+app.use(body-parser.urlencoded({'extended': 'false'}));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.use(express.static('./public'));
+app.use('/api/book', book);
+app.use(function (req, res, next) {
+	var err = new Error('not found');
+	err.status = 404;
+	next(err);
+	// body...
+});
 
+app.use(function (err, req, res, next) {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-todoController(app);
+	res.status(err.status || 500);
+	res.render('error');
+	// body...
+});
 
-app.listen(3000);
+module.exports = app;
